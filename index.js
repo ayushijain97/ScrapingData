0165a392955d735c5795af779d98bbf695305e0a
ayushijain97 <neeraj_jain@apple.com>
2021-08-01 05:31:24 +0530

    Removed blank href and titles

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
-const puppeteer = require("puppeteer");
+const express = require('express')
+const puppeteer = require("puppeteer")
+const app = express()
+const port = 3000
 
-(async () => {
-//   const browser = await puppeteer.launch({devtools: true});
-  const browser = await puppeteer.launch();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/data", async function(req, res) {
+  let data = await scrapeSaavn();
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
+
+async function scrapeSaavn() {
+    const browser = await puppeteer.launch({devtools: true});
+  // const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
   await page.goto("https://www.jiosaavn.com");
 
-//   await delay(30000);
+  //   await delay(30000);
   console.log("Waiting.....");
 
   await page.waitForSelector(".c-drag");
   await page.waitForSelector(".o-block__link");
   let urlsFetched = await page.$$eval(".o-block__link", (links) => {
+    // links = links.filter((link) => link.href !== '');
     const urls = links.map((link) => {
-        // debugger;
+      // debugger;
         const innerHTMLOfImg = link.innerHTML;
-        var wrapper= document.createElement('div');
+        var wrapper = document.createElement("div");
         wrapper.innerHTML = innerHTMLOfImg;
-        return {href: link.href, img: wrapper.firstElementChild.src, title: ''};
+        return { href: link.href, img: wrapper.firstElementChild.src, title: "" };
     });
     return urls;
   });
-
-  console.log(urlsFetched);
+  // console.log(urlsFetched);
 
   let titlesObjects = await page.$$eval(".u-color-js-gray", (titles) => {
+    debugger;
     let urls = titles.map((title, index) => {
-      return {title: title.title };
+      return { title: title.title };
     });
     return urls;
   });
-  console.log(titlesObjects.length);
+  // console.log(titlesObjects.length);
   for (let i = 0; i < urlsFetched.length; i++) {
     urlsFetched[i].title = titlesObjects[i].title;
   }
-
-  console.log(urlsFetched);
-  
+  urlsFetched = urlsFetched.filter((url) => !!url.href);
+  // console.log(urlsFetched);
   await browser.close();
+  return urlsFetched;
 
   function delay(time) {
     return new Promise(function (resolve) {
       setTimeout(resolve, time);
     });
   }
-})();
+};
 
 
